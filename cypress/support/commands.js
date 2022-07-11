4214bce1f4bf0948ecd9e414ca2de22fa1946f6e
oseiasal <oseias.nascimento@kainos.com.br>
2022-07-11 09:59:20 -0300

    comando customizado adicionado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,7 +27,11 @@
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 });
+
+Cypress.Commands.add('goToPage', (page) => {
+    cy.visit(page)
+})
 
--- cypress/support/page_objects/product-to-cart.page.js
+++ cypress/support/page_objects/product-to-cart.page.js
@@ -1,7 +1,7 @@
 class ProductToCart {
 
     addProduct({ nome, size, color }) {
-        cy.visit('/')
+        cy.goToPage('/')
         cy.get('[class="product-block grid"]').contains(nome).click()
         cy.get('.single_add_to_cart_button').click()
 
@@ -9,8 +9,6 @@ class ProductToCart {
         cy.get('.button-variable-item-' + color).click()
         cy.get('.single_add_to_cart_button').click()
     }
-
-
 
 }
 
